9ab92aa1b1d473e9cc69ae91c8583ad2f85592f1
Diyor-net <dgwww2222@gmail.com>
2026-06-20 08:09:25 +0500

    M

--- src/cfg.js
+++ src/cfg.js
@@ -1,1 +1,1 @@
-export const API_LINK = 'http://localhost:5000/api', SITE_LINK= "http://localhost:5000";
+export const API_LINK = 'https://api.olim-foundation.uz/api', SITE_LINK= "https://api.olim-foundation.uz";
